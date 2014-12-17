48efdb59ca31f382f7678911201d20b319713319
rootscope <datrootscope@gmail.com>
2014-12-16 19:07:31 -0700

    add threading start

--- src/net/rootscope/halfmana/Component.java
+++ src/net/rootscope/halfmana/Component.java
@@ -1,12 +1,20 @@
 package net.rootscope.halfmana;
 
-public class Component {
+public class Component implements Runnable{
 	
 	public static int width = 1920 / 2;
 	public static int height = 1080 / 2;
 	
 	private Thread thread;
 	
-	
+	public synchronized void start(){
+		thread = new Thread(this, "component");
+		thread.start();
+	}
+
+	@Override
+	public void run() {
+		
+	}
 
 }
