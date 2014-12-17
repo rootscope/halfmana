aa4b4b5064992300930292e1e0e721510dae95bd
rootscope <datrootscope@gmail.com>
2014-12-16 19:41:40 -0700

    add window/jframe config

--- src/net/rootscope/halfmana/Component.java
+++ src/net/rootscope/halfmana/Component.java
@@ -1,18 +1,37 @@
 package net.rootscope.halfmana;
 
-public class Component implements Runnable{
-	
+import java.awt.Canvas;
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+
+public class Component extends Canvas implements Runnable{
+	private static final long serialVersionUID = 1L;
+
 	public static int width = 1920 / 2;
 	public static int height = 1080 / 2;
+	public static int scale = 1;
 	
 	private Thread thread;
+	private JFrame frame;
+	private boolean running = false;
+	
+	public Component(){
+		frame = new JFrame();
+		Dimension size = new Dimension(width*scale, height*scale);
+		setPreferredSize(size);
+		
+		
+	}
 	
 	public synchronized void start(){
+		running = true;
 		thread = new Thread(this, "component");
 		thread.start();
 	}
 	
 	public synchronized void stop(){
+		running = false;
 		try {
 			thread.join();
 		} catch (InterruptedException e) {
@@ -21,8 +40,24 @@ public class Component implements Runnable{
 	}
 
 	@Override
-	public void run() {
+	public void run(){
+		while(running){
+			
+		}
+	}
+	
+	public static void main(String[] args){
+		Component component = new Component();
+
+		component.frame.setResizable(false);
+		component.frame.setTitle("halfmana");
+		component.frame.add(component);
+		component.frame.pack();
+		component.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		component.frame.setLocationRelativeTo(null);
+		component.frame.setVisible(true);
 		
+		component.start();
 	}
 
 }
