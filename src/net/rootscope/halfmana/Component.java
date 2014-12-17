1bc4013242ecbfe399911a1b49c7a0e02608c71d
rootscope <datrootscope@gmail.com>
2014-12-16 20:27:51 -0700

    init screen

--- src/net/rootscope/halfmana/Component.java
+++ src/net/rootscope/halfmana/Component.java
@@ -9,6 +9,8 @@ import java.awt.image.DataBufferInt;
 
 import javax.swing.JFrame;
 
+import net.rootscope.halfmana.graphics.Screen;
+
 public class Component extends Canvas implements Runnable{
 	private static final long serialVersionUID = 1L;
 
@@ -19,13 +21,17 @@ public class Component extends Canvas implements Runnable{
 	private boolean running = false;
 	private Thread thread;
 	private JFrame frame;
+	private Screen screen;
+
 	private BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
 	private int[] pixels = ((DataBufferInt)image.getRaster().getDataBuffer()).getData();
 	
 	public Component(){
-		frame = new JFrame();
 		Dimension size = new Dimension(width*scale, height*scale);
 		setPreferredSize(size);
+		
+		screen = new Screen(width, height);
+		frame = new JFrame();
 	}
 	
 	public synchronized void start(){
---
+++ src/net/rootscope/halfmana/graphics/Screen.java
@@ -0,0 +1,15 @@
+package net.rootscope.halfmana.graphics;
+
+public class Screen{
+	private int width;
+	private int height;
+	
+	public int[] pixels;
+	
+	public Screen(int width, int height){
+		this.width = width;
+		this.height = height;
+		pixels = new int[width * height];
+	}
+
+}
