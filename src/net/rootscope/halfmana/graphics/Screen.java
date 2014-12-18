66675296dbc91808bddc090dff037fab4d1c5970
rootscope <datrootscope@gmail.com>
2014-12-17 22:07:50 -0700

    pixel draw stuff

--- src/net/rootscope/halfmana/graphics/Screen.java
+++ src/net/rootscope/halfmana/graphics/Screen.java
@@ -6,16 +6,29 @@ public class Screen{
 	
 	public int[] pixels;
 	
+	int time;
+	int count;
+	
 	public Screen(int width, int height){
 		this.width = width;
 		this.height = height;
 		pixels = new int[width * height];
 	}
 	
+	public void clear(){
+		for(int i = 0; i < pixels.length; i++){
+			pixels[i] = 0;
+		}
+	}
+	
 	public void render(){
+		count++;
+		if(count % 10 == 0){
+			time++;
+		}
 		for(int y = 0; y < height; y++){
 			for(int x = 0; x < width; x++){
-				pixels[x + y * width] = 0xff00ff;
+				pixels[time + time * width] = 0xff00ff;
 			}
 		}
 
