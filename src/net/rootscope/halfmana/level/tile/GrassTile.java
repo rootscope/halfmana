34dbaea755ad6896d3efdf399c83ec97a66ffee7
rootscope <datrootscope@gmail.com>
2014-12-18 22:15:24 -0700

    add grass tile

---
+++ src/net/rootscope/halfmana/level/tile/GrassTile.java
@@ -0,0 +1,11 @@
+package net.rootscope.halfmana.level.tile;
+
+import net.rootscope.halfmana.graphics.Sprite;
+
+public class GrassTile extends Tile{
+
+	public GrassTile(Sprite sprite) {
+		super(sprite);
+	}
+
+}
